5a678a5f186f3b5b1f894fd9dba994ece26f3fee
wang2hao <378619897@qq.com>
2024-01-22 09:27:30 +0800

    update: update api

--- src/api/server-api.js
+++ src/api/server-api.js
@@ -1,9 +1,35 @@
 import service from "@/utils/request.js"
 
-export const captcha = (data) => {
+// ticker列表
+export const tickerList = (data) => {
   return service({
-    url: "/base/captcha",
+    url: `/v1/tickerList?page=${data.page}&pageSize=${data.pageSize}&type=${data.type}`,
+    method: "get",
+  })
+}
+
+// 最新的铸造费用
+export const gasCountLatest = (data) => {
+  return service({
+    url: `/gasCountLatest?addr=${data.addr}`,
+    method: "get",
+  })
+}
+
+// 验证是否存在这个token
+export const verifyToken = (data) => {
+  return service({
+    url: `/verifyToken`,
     method: "post",
-    data: data,
+    data,
+  })
+}
+
+// 创建一个订单
+export const createOrder = (data) => {
+  return service({
+    url: `/createOrder`,
+    method: "post",
+    data,
   })
 }
